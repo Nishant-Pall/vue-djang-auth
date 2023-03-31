ccd5cfedb7bbdad5d87413f304b3471e2a9cc073
prhyme <palln6935@gmail.com>
2023-04-01 01:30:03 +0530

    Setup client

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+	<v-container>
+		<v-row class="text-center">
+			<div>HELLO WORLD</div>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+};
+</script>
