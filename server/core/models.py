1fce4e8adf0869c1a1345556f657c058382601e1
prhyme <palln6935@gmail.com>
2023-04-05 19:16:50 +0530

    Make username as primary key

--- server/core/models.py
+++ server/core/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=30)
