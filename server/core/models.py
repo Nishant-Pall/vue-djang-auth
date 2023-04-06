7d9819ae4ce6efc81c1f3f944b838f3036508a68
prhyme <palln6935@gmail.com>
2023-04-06 19:33:58 +0530

    Create end point to register new user

--- server/core/models.py
+++ server/core/models.py
@@ -6,6 +6,3 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.username
--- server/core/views.py
+++ server/core/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from core.serializers import UserSerializer
 
 # Create your views here.
 
 
-@api_view(['GET'])
-def say_hello(request):
-    users = User.objects.all()
-    return Response('HELLO hi hey')
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response('USER CREATED', status.HTTP_201_CREATED)
+    return Response('USER NOT CREATED, BAD REQUEST', status.HTTP_201_BAD_REQUEST)
