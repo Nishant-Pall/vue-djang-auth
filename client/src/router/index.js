da2aad374db0f7698f2b4a70ffe1ec762b70b5ee
prhyme <palln6935@gmail.com>
2023-04-16 21:43:04 +0530

    Redirect to login if unsuccesful signup

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import SignupView from "../views/SignupView.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -32,6 +33,15 @@ const router = new VueRouter({
 	mode: "history",
 	base: process.env.BASE_URL,
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	const isProtected = to.name === "home";
+	if (isProtected && store.state.currentUser.id === "") {
+		next({
+			path: "/login",
+		});
+	} else next();
 });
 
 export default router;
