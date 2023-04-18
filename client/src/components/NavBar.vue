bfec460b34218d80bb1db84660bc519cca4d19f8
prhyme <palln6935@gmail.com>
2023-04-18 19:27:56 +0530

    Add register buttons to navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -7,8 +7,11 @@
 			</template>
 		</v-snackbar>
 		<v-toolbar flat>
-			<v-toolbar-title>Yo</v-toolbar-title>
+			<v-toolbar-title>Yo {{ this.$store.state.currentUser?.username ?? "" }}</v-toolbar-title>
 			<v-spacer></v-spacer>
+			<v-btn v-if="!signedIn" @click="onRegister" color="success">
+				{{ this.$route.name === "signup" ? `Login in` : `Sign up` }}
+			</v-btn>
 			<v-btn v-if="signedIn" @click="signOut" text dark color="red">Sign out</v-btn>
 		</v-toolbar>
 	</nav>
@@ -25,6 +28,13 @@ export default {
 	methods: {
 		signOut() {
 			this.$store.dispatch("clearUser");
+		},
+		onRegister() {
+			if (this.$route.name === "signup") {
+				this.$router.push("login");
+			} else if (this.$route.name === "login") {
+				this.$router.push("signup");
+			}
 		},
 	},
 	computed: {
